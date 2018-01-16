5a62faa6dacb5f85c5d101c0c196928a3c050ab9
Eugene Khom <homyakny@gmail.com>
2018-01-16 01:59:50 -0500

    added tweeter, but can be called only by node liri.js tweets

--- liri.js
+++ liri.js
@@ -1,4 +1,35 @@
+// Create vars with all the information
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var request = require("request");
+var fs = require("fs");
+var inquirer = require('inquirer');
+
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+
+var commandLine = process.argv[2];
+
+function twitter() {
+
+    var client = new Twitter(keys.twitter);
+
+    var params = {
+        screen_name: 'Boot Camp HW'
+    };
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function (element) {
+                console.log(`================================================\n${element.text}\n================================================\n`);
+            });
+        }
+    });
+};
+
+switch (commandLine) {
+    case "tweets":
+        twitter();
+        break;
+}
